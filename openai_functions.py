419f88955fda899f1b59212ef1f9f1065f4346fd
Antony Johnson Cruz <antonycjohnson@gmail.com>
2024-08-29 14:20:40 +0200

    Update openai_functions.py

--- openai_functions.py
+++ openai_functions.py
@@ -3,24 +3,178 @@ from datetime import datetime
 from pymongo import MongoClient
 from typing import Optional, List
 from faker import Faker
+from dotenv import load_dotenv
+import os
 
-
+# load_dotenv() reads the .env file and sets the environment variables.
+load_dotenv()
 
 # Setup Faker and MongoDB connection
 fake = Faker()
 
-# MongoDB Connection Setup
-mongoclient = MongoClient("mongodb+srv://antjohns:PASSWORDHERE@cluster0.cw6ev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = mongoclient["payments"]  # Replace with your database name
-collection = db["transaction"]
+mongo_pwd = os.getenv("MONGO_PWD")
 
-function_create_transaction = {
-    "name": "create_transaction",
-    "description": "Create or Enter a new payment transaction in user transaction table of the bank.",
+# MongoDB Connection Setup
+mongoclient = MongoClient("mongodb+srv://antjohns:"+mongo_pwd+"@cluster0.cw6ev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = mongoclient["payments"]  # Replace with your database name
+collection = db["debit_transaction"]
+
+
+function_search_transactions = {
+    "name": "search_transactions",
+    "description": "Search for user payment transaction based on various criteria most of them are optional. The transactions are debit ones which are the transactions where the user is the sender.",    
     "parameters": {
         "type": "object",
         "properties": {
-           
+            
+            "date_from": {
+                "type": "string",
+                "format": "date-time",
+                "description": "The start date to filter transactions, formatted as an ISO 8601 datetime string.",
+            },
+            
+            "date_to": {
+                "type": "string",
+                "format": "date-time",
+                "description": "The end date to filter transactions, formatted as an ISO 8601 datetime string.",
+            },
+            "status": {
+                "type": "string",
+                "description": "The status of the transaction (e.g., 'completed', 'pending', 'failed').",
+            },
+            "currency": {
+                "type": "string",
+                "description": "The currency used in the transaction (e.g., 'CHF').",
+            },
+            "category": {
+                "type": "string",
+                "description": "The category of the transaction (e.g., 'Groceries', 'Shopping', 'Bills').",
+            },
+            "payment_channel": {
+                "type": "string",
+                "description": "The payment channel used in the transaction (e.g., 'Domestic', 'SEPA', 'International').",
+            },       
+            "receiver": {
+                "type": "string",
+                "description": "The name of the receiver or beneficiary of the transaction.",
+            },
+            "receiver_accountnumber": {
+                "type": "string",
+                "description": "The account number of the receiver in the transaction in IBAN format.",
+            },
+            "min_amount": {
+                "type": "number",
+                "description": "The minimum transaction amount to filter.",
+            },
+            "max_amount": {
+                "type": "number",
+                "description": "The maximum transaction amount to filter.",
+            },
+            "payment_reason": {
+                "type": "string",
+                "description": "A regex pattern to filter transactions based on the payment reason. The search is case-insensitive.",
+            },
+            "limit": {
+                "type": "integer",
+                "description": "The maximum number of transactions to return. Defaults to 10 if not provided.",               
+            },
+
+        },
+        "required": [],
+    },
+}
+
+def search_transactions(
+    transaction_id: Optional[str] = None,
+    date_from: Optional[datetime] = None,
+    date_to: Optional[datetime] = None,
+    status: Optional[str] = None,
+    currency: Optional[str] = None,
+    category: Optional[str] = None,
+    payment_channel: Optional[str] = None,
+    receiver: Optional[str] = None,
+    receiver_accountnumber: Optional[str] = None,
+    min_amount: Optional[float] = None,
+    max_amount: Optional[float] = None,
+    payment_reason: Optional[str] = None,
+    limit: Optional[int] = 10  # Default limit to the last 10 transactions
+) -> List[dict]:
+    
+    # Build the query dictionary dynamically
+    print(f"Searching transactions with params: {locals()}")
+    query = {}    
+    
+    if date_from and date_to:
+        query["txndate"] = {"$gte": date_from.isoformat(), "$lte": date_to.isoformat()}
+    elif date_from:
+        query["txndate"] = {"$gte": date_from.isoformat()}
+    elif date_to:
+        query["txndate"] = {"$lte": date_to.isoformat()}
+    if status:
+        query["status"] = status
+    if transaction_id:
+        query["transaction_id"] = transaction_id
+    if currency:
+        query["currency"] = currency
+    if category:
+        query["category"] = category
+    if payment_channel:
+        query["payment_channel"] = payment_channel
+    if receiver:
+        query["receiver"] = receiver
+    if receiver_accountnumber:
+        query["receiver_accountnumber"] = receiver_accountnumber
+    if min_amount and max_amount:
+        query["amount"] = {"$gte": min_amount, "$lte": max_amount}
+    elif min_amount:
+        query["amount"] = {"$gte": min_amount}
+    elif max_amount:
+        query["amount"] = {"$lte": max_amount}
+    if payment_reason:
+        query["payment_reason"] = {"$regex": payment_reason, "$options": "i"}
+    if limit:
+        limit = int(limit)
+    else:
+        limit = 10
+
+
+    # Execute the query
+    results = list(collection.find(query).sort("txndate", -1).limit(limit))
+    return results
+
+def create_transaction(       
+    amount: float,
+    currency: str,    
+    receiver: str,
+    receiver_accountnumber: str,
+    payment_reason: Optional[str] = None
+) -> dict:
+    # Build the transaction dictionary
+    transaction = {
+        "userid": "user123",        
+        "status": "completed",
+        "txndate": datetime.now().isoformat(),
+        "amount": amount,       
+        "currency": currency,
+        "category": "Other",
+        "payment_channel": "Domestic",
+        "receiver": receiver,
+        "receiver_accountnumber": receiver_accountnumber,
+        "payment_reason": payment_reason
+    }
+
+    # Insert the transaction into the MongoDB collection
+    result = collection.insert_one(transaction)
+    transaction["_id"] = str(result.inserted_id)
+    
+    return transaction
+
+function_create_transaction = {
+    "name": "create_transaction",
+    "description": "Create a new debit payment transaction for user. This is a debit transaction where the user is the sender.",
+    "parameters": {
+        "type": "object",
+        "properties": {           
             "amount": {
                 "type": "number",
                 "description": "The amount of the transaction.",
@@ -46,142 +200,6 @@ function_create_transaction = {
     },
 }
 
-function_search_transactions = {
-    "name": "search_transactions",
-    "description": "Search for transactions in the MongoDB collection based on various criteria.",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "userid": {
-                "type": "string",
-                "description": "The user ID associated with the transaction.",
-            },
-            "user_accountnumber": {
-                "type": "string",
-                "description": "The account number of the user associated with the transaction.",
-            },
-            "date_from": {
-                "type": "string",
-                "format": "date-time",
-                "description": "The start date to filter transactions, formatted as an ISO 8601 datetime string.",
-            },
-            "creditdebit": {
-                "type": "string",
-                "description": "The credit or debit type of the transaction. Debit means money is taken from the account, and credit means money is added to the account. (e.g., 'credit', 'debit')",               
-                 
-            },
-            "date_to": {
-                "type": "string",
-                "format": "date-time",
-                "description": "The end date to filter transactions, formatted as an ISO 8601 datetime string.",
-            },
-            "status": {
-                "type": "string",
-                "description": "The status of the transaction (e.g., 'completed', 'pending', 'failed').",
-            },
-            "currency": {
-                "type": "string",
-                "description": "The currency used in the transaction (e.g., 'USD', 'EUR').",
-            },
-            "transactionmode": {
-                "type": "string",
-                "description": "The mode of the transaction (e.g., 'online', 'offline', 'mobile').",
-            },
-            "receiver": {
-                "type": "string",
-                "description": "The name of the receiver in the transaction.",
-            },
-            "min_amount": {
-                "type": "number",
-                "description": "The minimum transaction amount to filter.",
-            },
-            "max_amount": {
-                "type": "number",
-                "description": "The maximum transaction amount to filter.",
-            },
-            "payment_reason": {
-                "type": "string",
-                "description": "A regex pattern to filter transactions based on the payment reason. The search is case-insensitive.",
-            }
-        },
-        "required": [],
-    },
-}
-
-def search_transactions(
-    userid: Optional[str] = None,
-    user_accountnumber: Optional[str] = None,
-    date_from: Optional[datetime] = None,
-    date_to: Optional[datetime] = None,
-    status: Optional[str] = None,
-    currency: Optional[str] = None,
-    creditdebit: Optional[str] = None,
-    transactionmode: Optional[str] = None,
-    receiver: Optional[str] = None,
-    min_amount: Optional[float] = None,
-    max_amount: Optional[float] = None,
-    payment_reason: Optional[str] = None
-) -> List[dict]:
-    
-    # Build the query dictionary dynamically
-    query = {}    
-    
-    if date_from and date_to:
-        query["txndate"] = {"$gte": date_from.isoformat(), "$lte": date_to.isoformat()}
-    elif date_from:
-        query["txndate"] = {"$gte": date_from.isoformat()}
-    elif date_to:
-        query["txndate"] = {"$lte": date_to.isoformat()}
-    if status:
-        query["status"] = status
-    if currency:
-        query["currency"] = currency
-    if creditdebit:
-        query["creditdebit"] = creditdebit
-    if transactionmode:
-        query["transactionmode"] = transactionmode
-    if receiver:
-        query["receiver"] = receiver
-    if min_amount and max_amount:
-        query["amount"] = {"$gte": min_amount, "$lte": max_amount}
-    elif min_amount:
-        query["amount"] = {"$gte": min_amount}
-    elif max_amount:
-        query["amount"] = {"$lte": max_amount}
-    if payment_reason:
-        query["payment_reason"] = {"$regex": payment_reason, "$options": "i"}
-
-    # Execute the query
-    results = list(collection.find(query))
-    return results
-
-def create_transaction(       
-    amount: float,
-    currency: str,    
-    receiver: str,
-    receiver_accountnumber: str,
-    payment_reason: Optional[str] = None
-) -> dict:
-    # Build the transaction dictionary
-    transaction = {
-        "userid": 282493,
-        "user_accountnumber": "10203040",
-        "status": "completed",
-        "txndate": datetime.now().isoformat(),
-        "amount": amount,
-        "creditdebit": "debit",
-        "currency": currency,
-        "transactionmode": "aibot",
-        "receiver": receiver,
-        "receiver_accountnumber": fake.iban(),
-        "payment_reason": payment_reason
-    }
-
-    # Insert the transaction into the MongoDB collection
-    result = collection.insert_one(transaction)
-    transaction["_id"] = str(result.inserted_id)
-    
-    return transaction
 
 def call_search_transactions (response, messages):
     # Extract the tool call response
@@ -209,7 +227,7 @@ def call_search_transactions (response, messages):
 
     function_call_result_message = {
         "role": "tool",
-        "content": json.dumps(results[0]),
+        "content": json.dumps(results),
          "tool_call_id": toolcall_response_message.tool_calls[0].id
     }
     messages.append(toolcall_response_message)
